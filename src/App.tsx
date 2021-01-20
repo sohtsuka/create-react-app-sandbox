c46387798aac7b310b82f3e2d9908341ed28a332
Satoshi Ohtsuka <sohtsuka1317@gmail.com>
2021-01-20 13:46:36 +0000

    Use styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppLogo = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: App-logo-spin infinite 20s linear;
+  }
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const AppHeader = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppContainer>
+      <AppHeader>
+        <AppLogo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
   );
 }
 
